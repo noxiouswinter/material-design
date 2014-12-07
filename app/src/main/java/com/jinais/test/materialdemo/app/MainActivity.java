325348361e0c7ab6431421458eb881e00292a2c5
Jinais Ponnampadikkal Kader <jpkader@amplify.com>
2014-12-07 05:10:00 -0500

    Adding ActionBar with menu item and material based color. Uses AppCompat for theming and menu. Theme.AppCompat switches to Material theme on android L. The colorPrimaryDark works for the notification bar only for Android L. colorPrimary works for the Action bar for all API Levels.

--- app/src/main/java/com/jinais/test/materialdemo/app/MainActivity.java
+++ app/src/main/java/com/jinais/test/materialdemo/app/MainActivity.java
@@ -2,6 +2,10 @@ package com.jinais.test.materialdemo.app;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -9,5 +13,28 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                Log.d("DEBUG", "search");
+                return true;
+            case R.id.action_settings:
+                Log.d("DEBUG", "settings");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
